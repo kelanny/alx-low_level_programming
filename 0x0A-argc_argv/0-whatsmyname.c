d43dc434843db15f19d61671a57c172151ec2409
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-06-14 12:23:42 -0700

    Added task 0 which is 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - The program prints its name
+ * @argc: input count variable
+ * @argv: input vector variable
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,4 @@
-0x0A-ar_c_argv
+0x0A-argc_argv
+Task: 0. It ain't what they call you, it's what you answer to
+> A program that prints its name, followed by a new line.
+
