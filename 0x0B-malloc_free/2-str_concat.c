0f7edd1e6c517e6a84f3c2b04dde01c47ef300dc
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-06-14 18:52:45 -0700

    Added task 2: 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * str_concat - The function concatenate two strings of given sizes.
+ * @s1: First input string to concatenate
+ * @s2: Second input string to concatenate
+ *
+ * Return: Concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int a = 0, b = 0, c = 0, d = 0;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[a])
+	{
+		a++;
+	}
+	while (s2[b])
+	{
+		b++;
+	}
+	d = a + b;
+	s = malloc((sizeof(char) * d) + 1);
+	if (s == NULL)
+		return (NULL);
+	b = 0;
+
+	while (c < d)
+	{
+		if (c <= a)
+			s[c] = s1[c];
+		if (c >= a)
+		{
+			s[c] = s2[b];
+			b++;
+		}
+		c++;
+	}
+	s[c] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -5,3 +5,8 @@ Task: 0. Float like a butterfly, sting like a bee
 
 Task: 1. The woman who has no imagination has no wings
 > Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+
+Task: 2. He who is not courageous enough to take risks will accomplish nothing in life
+> Write a function that concatenates two strings.
+
+
