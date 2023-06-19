20756e67c0d87ffe8bd71fb537eea5fa539a2043
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-06-19 15:22:58 -0700

    Added a task dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+Project >>> 0x0E-structures_typedef
+
+Task: 0. Poppy
+> Define a new type struct dog with the following elements:
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: This struct represent a data type called dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
