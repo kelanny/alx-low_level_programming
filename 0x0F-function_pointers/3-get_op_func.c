e834cbde43904be70829d61c05d2872e356a8a1e
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-06-21 18:14:30 -0700

    Added task 3 which Write a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - Select the appropriate fuction dependind on the operator s
+ * @s: The operator passed
+ *
+ * Return: A pointer to the choosen function.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Returns a sum of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtract two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiply two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divide two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: A Division of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Finds a modulo of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: A remaider of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
