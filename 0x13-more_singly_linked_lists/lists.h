10923611592a46546a43d7ae53b34970df94b365
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-07-04 11:29:26 -0700

    Added task 1-listint_len.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: Integer
@@ -15,5 +18,5 @@ typedef struct listint_s
         struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
-
+size_t listint_len(const listint_t *h);
 #endif
