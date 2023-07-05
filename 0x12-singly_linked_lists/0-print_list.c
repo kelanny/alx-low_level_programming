e799ed07e32949098250eeb23560ded24da11cf7
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-07-05 09:00:44 -0700

    Edited task 0-print_list.c for bugs

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,19 +10,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t count = 0;
 
 	while (h)
 	{
 		if (!h->str)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
-			i++;
-		}
-		h++;
+		h = h->next;
+		count++;
 	}
-	return (i);
+	return (count);
 }
