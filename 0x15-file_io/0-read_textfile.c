82cc1452c3d5c9b26281bb4110e2d9e9b3afea57
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-07-14 16:33:18 -0700

    Added task 0-readJ_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX stdout.
+ * @filename: A file path to the file.
+ * @letters: Number of bytes to read.
+ *
+ * Return: No of letters it reads and printed otherwise returns 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int fp;
+	ssize_t length = 0;
+
+	if (filename == NULL)
+		return (0);
+	fp = open("Requirescat", O_RDONLY);
+
+	if (fp == -1)
+		return (0);
+
+       	buffer = malloc(letters * sizeof(char));
+	length = read(fp, buffer, letters);
+	printf("%s\n", buffer);
+	free(buffer);
+	return (length);
+}
