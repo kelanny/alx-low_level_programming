fe7856319f1bbfbb622583e998570b88baa0d240
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-07-22 14:37:41 -0700

    Revised task 0 and edited 0-read_textfile.c for bugs

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-       	buffer = malloc(letters * sizeof(char));
+	buffer = malloc(letters * sizeof(char));
 	readbytes = read(fp, buffer, letters);
 	writebytes = write(STDOUT_FILENO, buffer, readbytes);
 	if (writebytes == -1 || readbytes != writebytes)
