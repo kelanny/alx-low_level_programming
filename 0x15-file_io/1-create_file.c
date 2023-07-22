0a8b04fb020d6f6bc3dcc9e0e3d1a79a9623b42a
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-07-22 15:04:09 -0700

    Reviewed task 1 for corrections and edited file 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,16 +32,21 @@ int create_file(const char *filename, char *text_content)
 {
 	int fp;
 	ssize_t bytes_written;
-	int len = str_len(text_content);
 
-	fp = open(filename, O_WRONLY);
-	if (fp == NULL)
+	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		close(fp);
-	bytes_written = write(fp, text_content, len);
-	if (bytes_written == -1)
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fp == -1)
 		return (-1);
+	if (text_content != NULL)
+	{
+		bytes_written = write(fp, text_content, (str_len(text_content) + 1));
+		if (bytes_written == -1)
+		{
+			close(fp);
+			return (-1);
+		}
+	}
 	close(fp);
 	return (1);
 }
