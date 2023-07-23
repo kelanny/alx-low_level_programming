6af14b24d0d5c175131ae3d72125517a7330c80b
Kelvin Chuchu <mburukelvin17@gmail.com>
2023-07-22 18:23:36 -0700

    Added task 3 file 3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,9 +8,9 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int _putchar(char c);
+#define MAX_LEN 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
-int str_len(const char *str);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
