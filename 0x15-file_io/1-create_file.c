9a1dd8d074f0cd460303e17fd9f7672f759d5d75
kelanny <mburukelvin17@gmail.com>
2023-08-07 15:16:19 -0700

    Edited task 1 file 1-create_file.c for bugs

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		close(fd);
@@ -38,5 +38,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(fd);
-	return (bytes);
+	return (1);
 }
