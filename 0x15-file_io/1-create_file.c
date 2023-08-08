40bcc61d61ef56cda2dcea6f316d8d515d7d9b8a
kelanny <mburukelvin17@gmail.com>
2023-08-07 19:33:05 -0700

    Edited create_file function for bugs.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,26 +15,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[txt_len] != '\0')
+			txt_len++;
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
-	while (text_content[txt_len] != '\0')
-	{
-		txt_len++;
-	}
-
 	bytes = write(fd, text_content, txt_len);
-	if (bytes == -1)
+	if (fd == -1 || bytes == -1)
 	{
-		close(fd);
 		return (-1);
 	}
 	close(fd);
